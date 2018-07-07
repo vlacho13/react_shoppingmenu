bd34155f19b231dc47c9a49a4ea8ba1078071d10
vlacho13 <striuncekf@hotmail.com>
2018-07-07 17:28:21 -0500

    Create-react-app, added bootstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Grid } from "react-bootstrap";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Grid className="purchase-card">
+          <h1>Hello</h1>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
